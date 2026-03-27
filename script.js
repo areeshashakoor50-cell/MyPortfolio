7e0eefdcd0bea5a8e0a8f8b77ad79b4c26752a40
Areesha Shakoor <areeshashakoor50@gmail.com>
2026-03-27 14:35:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// script.js
-// No functionality yet, ready for future enhancements
+let display = document.getElementById('display');
+let currentInput = '';
+
+function press(val) {
+    currentInput += val;
+    display.value = currentInput;
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput);
+        display.value = currentInput;
+    } catch (e) {
+        display.value = "Error";
+        currentInput = '';
+    }
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
